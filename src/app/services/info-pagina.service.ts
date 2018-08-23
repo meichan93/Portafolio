3a3cfa131f4c85cde6c7ff903d857a96864d9d0e
Mei <mei.lcs93@gmail.com>
2018-08-22 21:51:23 -0300

    Fin de sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -5,15 +5,28 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 @Injectable({
   providedIn: 'root'
 })
+
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
-    this.http.get('assets/data/data-pagina.json')
-    .subscribe( (resp: InfoPagina) => {
-      this.cargada = true;
-      this.info = resp;
-    });
-   }
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+  private cargarInfo() {
+      this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+  }
+  private cargarEquipo() {
+    this.http.get('https://angular-html-e1535.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+        this.equipo = resp;
+        console.log(resp);
+      });
+  }
 }
