2cd1d7e8c34f410237e0aa04bf576c3cb3aea263
Mei <mei.lcs93@gmail.com>
2018-11-01 23:49:11 -0300

    Ultimos cambios en la busqueda que no fueron subidos

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,23 +9,50 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-e1535.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: Producto[]) => {
-      this.productos = resp;
-      setTimeout(() => {
+    return new Promise((resolve, reject) => {
+      this.http.get('https://angular-html-e1535.firebaseio.com/productos_idx.json')
+      .subscribe((resp: Producto[]) => {
+        this.productos = resp;
         this.cargando = false;
-      }, 1000);
+        resolve();
+      });
     });
   }
 
-  getProducto( id: string ) {
-    return this.http.get(`https://angular-html-e1535.firebaseio.com/productos/${ id }.json`);
+  getProducto(id: string) {
+    return this.http.get(`https://angular-html-e1535.firebaseio.com/productos/${id}.json`);
     // para insertar una variable en un url, se deben poner--> `url` e insertar la variable dentro ${id} [Example]
+  }
+
+  buscarProducto(termino: string) {
+    if (this.productos.length === 0) {
+      this.cargarProductos().then(() => {
+        // ejecutar despues de tener productos
+        // app filtro
+        this.filtrarProductos(termino);
+      });
+    } else {
+      this.filtrarProductos(termino);
+    }
+    this.productosFiltrado = this.productos.filter(producto => {
+      return true;
+    });
+    console.log(this.productosFiltrado);
+  }
+
+  private filtrarProductos( termino: string) {
+    console.log(this.productos);
+    this.productos.forEach( prod => {
+      if ( prod.categoria.indexOf( termino ) >= 0) {
+        this.productosFiltrado.push ( prod );
+      }
+    });
   }
 }
